61bf45f55b7e6ba5d61e45b79dea7d1adb06ce1b
pji0219 <pji219@naver.com>
2022-08-08 01:03:30 +0900

    style add cart 버튼 구현

--- js/menu1.js
+++ js/menu1.js
@@ -1,6 +1,7 @@
 const logo = document.querySelector('.logo');
 const header = document.querySelector('header');
 
+// 로고, 헤더 이벤트
 logo.addEventListener('mouseover', function () {
   header.style.transform = 'translate(0px, 0px)';
 });

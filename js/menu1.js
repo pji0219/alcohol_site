dd77f5c206d91c69b7d45b1e4a345a1b70afae9a
pji0219 <pji219@naver.com>
2022-08-08 19:12:08 +0900

    style: gsap을 이용한 풀영상 애니메이션 구현, 버튼 호버시 이동하는 효과 구현

--- js/menu1.js
+++ js/menu1.js
@@ -17,3 +17,23 @@ logo.addEventListener('mouseout', function () {
 header.addEventListener('mouseout', function () {
   header.style.transform = 'translate(0px, -100px)';
 });
+
+gsap.to('.full_video', {
+  xPercent: -60,
+  duration: 1,
+  delay: 4,
+});
+gsap.from('.alcohols', {
+  delay: 4.5,
+  duration: 1,
+  opacity: 0,
+});
+
+gsap.from('.alcohols', {
+  Xpercent: -40,
+  delay: 13, //13말고다른 숫자 설정해서 test 해보세요 단, 위의 delay보다는 크게 설정 하시는게 좋습니다!
+  duration: 1,
+});
+
+/* gsap.from('.alcohol_img', { y: -20, opacity: 0, duration: 1 });
+gsap.from('.title', { y: -20, opacity: 0, duration: 1, delay: 1 }); */

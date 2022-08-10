37f958d297995aa8cf7dc19a63226ab2a132864c
pji0219 <pji219@naver.com>
2022-08-10 20:03:24 +0900

    feat: 스크롤시 영상 전환 기능 구현

--- js/menu1.js
+++ js/menu1.js
@@ -18,6 +18,18 @@ header.addEventListener('mouseout', function () {
   header.style.transform = 'translate(0px, -100px)';
 });
 
+// 영상 효과
+
+// 처음 페이지 로드후 4초 후부터 스크롤 되게끔 하는 것
+const wrap = document.querySelector('.wrap');
+
+window.onload = () => {
+  setTimeout(() => {
+    wrap.style.overflow = 'visible';
+  }, 4000);
+};
+
+// 처음 페이지 로딩시 영상효과
 gsap.to('.full_video', {
   xPercent: -60,
   duration: 1,
@@ -29,11 +41,54 @@ gsap.from('.alcohols', {
   opacity: 0,
 });
 
-gsap.from('.alcohols', {
-  Xpercent: -40,
-  delay: 13, //13말고다른 숫자 설정해서 test 해보세요 단, 위의 delay보다는 크게 설정 하시는게 좋습니다!
-  duration: 1,
+// 페이지 스크롤할 때 영상 효과
+
+window.addEventListener('scroll', () => {
+  const scrollYpos = window.scrollY;
+  console.log(scrollYpos);
+
+  if (scrollYpos > 1000) {
+    gsap.to('.video2', {
+      xPercent: 100,
+      duration: 1,
+    });
+  }
+
+  if (scrollYpos > 1400) {
+    gsap.to('.video3', {
+      yPercent: -100,
+      duration: 1,
+    });
+  }
 });
 
-/* gsap.from('.alcohol_img', { y: -20, opacity: 0, duration: 1 });
-gsap.from('.title', { y: -20, opacity: 0, duration: 1, delay: 1 }); */
+// 좋아요 하트 모양 변경
+const emptyHearts = document.querySelectorAll('.empty_heart');
+const fillHearts = document.querySelectorAll('.fill_heart');
+
+// 채워진 하트로 변경, 이렇게 하는것을 권장
+for (let i = 0; i < emptyHearts.length; i++) {
+  //
+  emptyHearts[i].addEventListener('click', () => {
+    //
+    if (fillHearts[i].classList.contains('hide')) {
+      fillHearts[i].classList.remove('hide');
+      emptyHearts[i].classList.add('hide');
+    }
+  });
+}
+
+// 빈 하트로 변경 ( for문 중첩 ver ), 이렇게 하는건 비추
+for (let i = 0; i < fillHearts.length; i++) {
+  //
+  for (let j = 0; j < emptyHearts.length; j++) {
+    //
+    fillHearts[i].addEventListener('click', () => {
+      //
+      if (emptyHearts[j].classList.contains('hide')) {
+        emptyHearts[j].classList.remove('hide');
+        fillHearts[i].classList.add('hide');
+      }
+    });
+  }
+}

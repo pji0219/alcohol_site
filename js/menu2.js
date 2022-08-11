f7fb0a59eb935b76f435d644a3ecb2f0ea9d0652
UDITRO <pam0701@daum.net>
2022-08-11 20:59:09 +0900

    1. feat: Animation(CardFlipOnScroll), Flow word, SnowFall, BirdFly,    Direct btn to top 구현 2. docs: birth_video 추가
    - 스크롤에 따라 반응하는 flip되는 card
    - 눈 내리는 효과
    - 새 날아다니는 효과
    - 클릭 시 최상단으로 올라가는 버튼
    - birth 주류 추천 구역에 넣을 video src 추가
    
    -

--- js/menu2.js
+++ js/menu2.js
@@ -58,7 +58,7 @@ function hasScrolled() {
     lastScrollTop = st;
 }
 
-/* 하단 스크롤 위로 바로가기 버튼 */
+/* Direct btn to top*/
 const gotoTopEl = document.querySelector('#goto-top')
 window.addEventListener('scroll', function() {
   if (window.scrollY > 500) { // 페이지 스크롤 위치가 500px이 넘으면
@@ -77,3 +77,110 @@ gotoTopEl.addEventListener('click', function() { //스크롤 버튼 클릭시
     scrollTo: 0
   })
 })
+
+/* SCROLL FLIP-Animation  */
+class CardFlipOnScroll{
+    constructor(wrapper, sticky) {
+        this.wrapper = wrapper
+        this.sticky = sticky
+        this.cards = sticky.querySelectorAll('.card')
+        this.length = this.cards.length
+
+        this.start = 0
+        this.end = 0
+        this.step = 0
+    }
+    init(){
+        this.start = this.wrapper.offsetTop
+        this.end = this.wrapper.offsetTop + this.wrapper.offsetHeight - innerHeight
+        this.step = (this.end - this.start) / (this.length * 2)
+    }
+    animate(){
+        this.cards.forEach((card, i) => {
+            const s = this.start + this.step * i 
+            const e = s + this.step * (this.length + 1)
+
+            if(scrollY <= s){
+                card.style.transform = `
+                perspective(100vw)
+                translateX(100vw)
+                rotateY(180deg)
+                `
+            }else if(scrollY > s && scrollY <= e - this.step){
+                card.style.transform = `
+                perspective(100vw)
+                translateX(${100 - (scrollY - s) / (e - s) * 100}vw)
+                rotateY(180deg) `
+            }else if(scrollY > e - this.step && scrollY <= e){
+                card.style.transform = `
+                perspective(100vw)
+                translateX(${100 - (scrollY - s) / (e - s) * 100}vw)
+                rotateY(${180 - -(scrollY - (e - this.step)) / this.step * 180}deg)
+                `
+            }else if(scrollY > e){
+                card.style.transform = `
+                perspective(100vw)
+                translateX(0vw)
+                rotateY(0deg)
+                `
+            }
+        })
+    }
+}
+const mainContent1 = document.querySelector('.main-content-1')
+const sticky = document.querySelector(".sticky")
+const cardFlipOnScroll = new CardFlipOnScroll(mainContent1, sticky)
+cardFlipOnScroll.init()
+
+window.addEventListener('scroll', ()=>{
+    cardFlipOnScroll.animate()
+})
+window.addEventListener('resize', ()=>{
+    cardFlipOnScroll.init()
+})
+
+/* Flow word */
+const pTag1 = document.querySelector('.first-parallel')
+const pTag2 = document.querySelector('.second-parallel')
+
+const textArr1 = 'Alcohol Yummy Food Tasty Useful Fun'.split(' ')
+const textArr2 = 'Love Yummy Food Tasty Powerful Adorable'.split(' ')
+
+function initTexts(element, textArray){
+    textArray.push(...textArray)
+    for(let i = 0; i < textArray.length; i++){
+        element.innerText += `${textArray[i]}\u00A0\u00A0\u00A0\u00A0\u00A0`
+    }
+}
+initTexts(pTag1, textArr1)
+initTexts(pTag2, textArr2)
+
+let count1 = 0
+let count2 = 0
+
+function marqueeText(count, element, direction){
+    if(count > element.scrollWidth /2){
+        element.style.transform = `translateX(0)`
+        count = 0
+    }
+    element.style.transform = `translateX(${count * direction}px)`
+    return count
+}
+
+function animate(){
+    count1++
+    count2++
+
+    count1 = marqueeText(count1, pTag1, -1)
+    count2 = marqueeText(count2, pTag2, 1)
+
+    window.requestAnimationFrame(animate)
+}
+/* 함수 사용 */
+animate() 
+
+/* window.addEventListener('scroll', ()=> {
+    count1 += 15
+    count2 += 15
+}) */
+
